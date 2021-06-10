cc193ff06bb2662d9462a9068e5b7416bcdccf7d
Jiuqi Zhang <jz3337@nyu.edu>
2021-06-09 21:53:30 -0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,166 @@
+#define GL_SILENCE_DEPRECATION
+
+#ifdef _WINDOWS
+#include <GL/glew.h>
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define GL_GLEXT_PROTOTYPES 1
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+SDL_Window* displayWindow;
+bool gameIsRunning = true;
+
+ShaderProgram program;
+//ball
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
+
+//professor
+glm::mat4 viewMatrixP, modelMatrixP, projectionMatrixP;
+
+//ground
+glm::mat4 viewMatrixG, modelMatrixG, projectionMatrixG;
+float ballX = 0;
+float ballRotate = 0;
+float profX;
+float lastTicks = 0.0f;
+
+GLuint ballTextureID;
+GLuint profTextureID;
+GLuint groundTextureID;
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_image_free(image);
+    return textureID;
+}
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("Project1!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+
+#ifdef _WINDOWS
+    glewInit();
+#endif
+
+    glViewport(0, 0, 640, 480);
+
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
+
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+    //program.SetColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+    glUseProgram(program.programID);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    ballTextureID = LoadTexture("ball.png");
+    profTextureID = LoadTexture("ctg.png");
+    groundTextureID = LoadTexture("ground.png");
+    glEnable(GL_BLEND);
+    // Good setting for transparency
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+}
+
+void ProcessInput() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
+        }
+    }
+}
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+
+    ballX += 1.0f * deltaTime;
+    ballRotate += 90.0f * deltaTime;
+    profX += 1.0f * deltaTime;
+
+    modelMatrixP = glm::mat4(1.0f);
+    modelMatrixP = glm::translate(modelMatrixP, glm::vec3(-4.0f, -3.0f, 0.0f));
+    modelMatrixP = glm::translate(modelMatrixP, glm::vec3(4+3*sin(1.0*profX), 0.0f, 0.0f));
+    modelMatrix = glm::mat4(1.0f);
+
+    modelMatrixG = glm::mat4(1.0f);
+    modelMatrixG = glm::scale(modelMatrix, glm::vec3(6.0f, 3.0f, 1.0f));
+
+    modelMatrix = glm::translate(modelMatrixP, glm::vec3(0.0f, 1.0f, 0.0f));
+    modelMatrix = glm::rotate(modelMatrix, glm::radians(ballRotate), glm::vec3(0.0f, 0.0f, 1.0f));
+
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    program.SetModelMatrix(modelMatrixP);//prof
+    glBindTexture(GL_TEXTURE_2D, profTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    program.SetModelMatrix(modelMatrix);//ball
+    glBindTexture(GL_TEXTURE_2D, ballTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    program.SetModelMatrix(modelMatrixG);//ground
+    glBindTexture(GL_TEXTURE_2D, groundTextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDisableVertexAttribArray(program.positionAttribute);
+    glDisableVertexAttribArray(program.texCoordAttribute);
+
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+
+    Shutdown();
+    return 0;
+}
